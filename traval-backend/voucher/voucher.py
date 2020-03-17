54be9de008066eb85e68a4ac23e5f5451f7f1a66
Jack Phan <jack.phan.2018@sis.smu.edu.sg>
2020-03-18 04:26:02 +0800

    Update vouchers
    Refer to comments for wip

--- traval-backend/voucher/voucher.py
+++ traval-backend/voucher/voucher.py
@@ -46,80 +46,63 @@ class Vouchers(db.Model):
     def json(self):
         return {"id": self.id, "user_id": self.user_id, "order_id": self.order_id, "applicable_date": self.applicable_date, "status": self.status}
 
-
-@app.route("/vouchers")
+#UC2 - Traval Booking UI interaction (getting voucher info)
+@app.route("/vouchers/<string:order_id>")
 def get_all_voucher(order_id):
     all_voucher = Vouchers.query.filter_by(order_id=order_id).first()
     if all_voucher:
-        reply = {"id":id, "status":status}
+        reply = {"id":Vouchers.id, "status":Vouchers.status}
         return jsonify(reply)
-
     return jsonify({"message":"Voucher not found."}), 404
 
-def merchant_scan_voucher(id):
-    scaned_voucher = Vouchers.query.filter_by(id=id).first()
-    scan_status= scaned_voucher.status
-    if scaned_voucher:
-        reply = {"order_id":order_id, "status":status}
+#UC2 - Merchant Traval UI interaction (getting voucher info)
+@app.route("/vouchers/merchant/<string:vouchers_id>")
+def merchant_scan_voucher(vouchers_id):
+    all_voucher = Vouchers.query.filter_by(vouchers_id=vouchers_id).first()
+    if all_voucher:
+        reply = {"id":Vouchers.order_id}
         return jsonify(reply)
-
     return jsonify({"message":"Voucher not found."}), 404
+
+#UC2 - AMQP to notification (automatically sent to notification api after voucher status changed)
+@app.route("/vouchers/merchant/redeem/<string:voucher_status>")
+def sent_to_notification():
+    hostname = "localhost" # default broker hostname. 
+    port = 5672 # default messaging port.
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+    channel = connection.channel()
+    # set up the exchange if the exchange doesn't exist
+    exchangename="voucher_direct"
+    channel.exchange_declare(exchange=exchangename, exchange_type='direct') 
+
 
 #unconfirm#########
-# @app.route("/voucher", methods=['PUT']) 
-# def merchant_update_redeem_status(id, status):
-#     voucher_status= Vouchers.query.filter_by(id=id).first()
-#     voucher_status[4]['status']=status
-#     data = request.get_json()
-#     item = CatalogItem(data)
+#UC2 - Merchant Traval UI interaction (Voucher status change)
+@app.route("/voucher", methods=['PUT']) 
+def merchant_redeem(id):
+    voucher_status= Vouchers.query.filter_by(id=id).first()
+    voucher_status[4]['status']=status
+    data = request.get_json()
+    item = Vouchers(data)
 
+#creating voucher (for testing)
+@app.route("/voucher", methods=['POST'])
+def create_voucher():
+    data = request.get_json()
+    item = Vouchers(**data)
 
-
-
-
-# @app.route("/voucher", methods=['POST'])
-# def create_voucher():
-    # data = request.get_json()
-    # item = CatalogItem(**data)
-
-    # if (CatalogItem.query.filter_by(id=id).first()):
-    #     return jsonify({"message": "A book with isbn13 '{}' already exists.".format(isbn13)}), 400
+    if (Vouchers.query.filter_by(id=Vouchers.id).first()):
+        return jsonify({"message": "A voucher with voucher id '{}' already exists.".format(Vouchers.id)}), 400
     
-    # try:
-    #     db.session.add(item)
-    #     db.session.commit()
-    # except:
-    #     return jsonify({"message": "An error occurred creating the catalog item."}), 500
-    # return jsonify(item.json()), 201
+    try:
+        db.session.add(item)
+        db.session.commit()
+    except:
+        return jsonify({"message": "An error occurred creating the voucher."}), 500
+    return jsonify(item.json()), 201
 
 
-# @app.route("/catalog-items")
-# def get_all():
-#     return jsonify({"catalog-items": [book.json() for book in CatalogItem.query.all()]})
-
-
-# @app.route("/catalog-items/<string:id>")
-# def find_by_id(id):
-#     item = CatalogItem.query.filter_by(id=id).first()
-#     if item:
-#         return jsonify(item.json())
-#     return jsonify({"message": "Item not found."}), 404
-
-# @app.route("/catalog-items", methods=['POST'])
-# def create_item():
-#     data = request.get_json()
-#     item = CatalogItem(**data)
-
-    # if (CatalogItem.query.filter_by(id=id).first()):
-    #     return jsonify({"message": "A book with isbn13 '{}' already exists.".format(isbn13)}), 400
-    
-    # try:
-    #     db.session.add(item)
-    #     db.session.commit()
-    # except:
-    #     return jsonify({"message": "An error occurred creating the catalog item."}), 500
-    # return jsonify(item.json()), 201
-    
+#UC1 - wip (cannot test cuz my rabbit died lol)
 def receivePayment():
 
     hostname = "localhost" # default broker hostname. 
@@ -151,7 +134,7 @@ def callback(channel, method, properties, body): # required signature for the ca
     result = processVoucher(json.loads(body))
 
     json.dump(result, sys.stdout, default=str) 
-    channel.basic_ack(delivery_tag=method.delivery_tag) # 
+    channel.basic_ack(delivery_tag=method.delivery_tag)
 
 #wip
 def processVoucher(data):
