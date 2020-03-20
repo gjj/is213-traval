cffbfbd37e705ba2a2906087c8d2a1433480e35d
Goi Jia Jian <jiajiannn@gmail.com>
2020-03-21 01:53:45 +0800

    Added bcrypt hashing for passwords

--- traval-backend/user-management/user.py
+++ traval-backend/user-management/user.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
+import bcrypt
 import uuid
 
 load_dotenv()
@@ -30,7 +31,7 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(90), nullable=False, unique=True)
     name = db.Column(db.String(90), nullable=False)
-    
+
     phone = db.Column(db.String(15), nullable=False)
 
     def __init__(self, id, guid, email, password, name, phone):
@@ -83,40 +84,75 @@ def find_by_phone(phone):
 
 
 @app.route("/users", methods=['POST'])
-def create_item():
+def create_user():
     errors = []
     data = request.get_json()
 
-    data.password = ''
+    guid = str(uuid.uuid1())
+    user = User(None, guid, **data)
+    
+    salt = bcrypt.gensalt()
+    user.password = bcrypt.hashpw(user.password.encode('utf-8'), salt) # https://stackoverflow.com/questions/43101904/bcrypt-salt-treating-byte-object-as-string-and-wont-hash-password
 
     # Validations
-    if (User.query.filter_by(id=user.id).filter_by(phone=user.phone).first()):
-        errors.append("A user with id '{}' already exists.".format(user.id))
-        # return jsonify({"message": "A user with id '{}' already exists.".format(user.id)}), 400
+    # if (User.query.filter_by(id=user.id).filter_by(phone=user.phone).first()):
+    #     errors.append("A user with id '{}' already exists.".format(user.id))
+    #     # return jsonify({"message": "A user with id '{}' already exists.".format(user.id)}), 400
 
     if (User.query.filter_by(email=user.email).first()):
-        errors.append(
-            "A user with email '{}' already exists.".format(user.email))
+        errors.append("A user with email '{}' already exists.".format(user.email))
         # return jsonify({"message": "A user with email '{}' already exists.".format(user.email)}), 400
 
     if (User.query.filter_by(phone=user.phone).first()):
-        errors.append(
-            "A user with phone '{}' already exists.".format(user.phone))
+        errors.append("A user with phone '{}' already exists.".format(user.phone))
         # return jsonify({"message": "A user with phone '{}' already exists.".format(user.phone)}), 400
 
     if len(errors) > 0:
+        print("Error", errors)
         return jsonify({"message": errors}), 400
 
     try:
         # make a UUID based on the host ID and current time, since we're not using for crypto, it's ok I guess
-        uuid = uuid.uuid1()
-        user = User(uuid, **data)
         db.session.add(user)
         db.session.commit()
-    except:
-        return jsonify({"message": "An error occurred creating the catalog item."}), 500
+    except e:
+        print("Error", e.__dict__['orig'])
+        return jsonify({"message": "An error occurred creating the user."}), 500
     return jsonify(user.json()), 201
 
+@app.route("/login", methods=['POST'])
+def login():
+    errors = []
+    data = request.get_json()
+
+    user = User(id=None, guid=None, name=None, phone=None, **data)
+    
+    # Validations
+    user_result = User.query.filter_by(email=user.email).first() # Lookup for a user with the given email
+    if (user_result):
+        if bcrypt.checkpw(user.password.encode('utf-8'), user_result.password.encode('utf-8')):
+            print("Password for {} matched".format(user.email))
+        else:
+            print("invalid")
+            errors.append("Invalid credentials.")
+    else:
+        errors.append("Invalid credentials.")
+        #user.password = bcrypt.hashpw(user.password.encode('utf-8'), salt) # https://stackoverflow.com/questions/43101904/bcrypt-salt-treating-byte-object-as-string-and-wont-hash-password
+
+    if len(errors) > 0:
+        print("Error", errors)
+        return jsonify({"message": errors}), 400
+
+    return jsonify({"status": "success", "token": "eyGE5F45S4F", "email": user_result.email, "name": user_result.name}), 201
+
+    # try:
+    #     # make a UUID based on the host ID and current time, since we're not using for crypto, it's ok I guess
+    #     db.session.add(user)
+    #     db.session.commit()
+    # except e:
+    #     print("Error", e.__dict__['orig'])
+    #     return jsonify({"message": "An error occurred creating the user."}), 500
+    # return jsonify(user.json()), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
