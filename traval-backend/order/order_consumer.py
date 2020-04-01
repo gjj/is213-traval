86a8affc913c54cb5e4dd55e920fe5249665a968
Goi Jia Jian <jiajiannn@gmail.com>
2020-04-01 23:36:14 +0800

    Added a new queue for notifications

--- traval-backend/order/order_consumer.py
+++ traval-backend/order/order_consumer.py
@@ -16,17 +16,22 @@ port = 3306
 db = 'traval_orders'
 user = 'traval'
 password = DATABASE_PASSWORD
-db = mysql.connector.connect(host=hostname, port=port, database=db, user=user, password=password)
+db = mysql.connector.connect(
+    host=hostname, port=port, database=db, user=user, password=password)
 
 print(' [order_consumer] Connecting to exchange...')
 hostname = "host.docker.internal"
 port = 5672
 exchangename = "traval.payments"
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=hostname, port=port))
 channel = connection.channel()
-channel.exchange_declare(exchange=exchangename, exchange_type='topic') # Creates the exchange
-channel.queue_declare(queue='order.update_status', durable=True) # Creates the queue
-channel.queue_bind(exchange=exchangename, queue='order.update_status', routing_key='order.update.*') # Bind queue to exchange, with the routing key
+channel.exchange_declare(exchange=exchangename,
+                         exchange_type='topic')  # Creates the exchange
+channel.queue_declare(queue='order.update_status',
+                      durable=True)  # Creates the queue
+channel.queue_bind(exchange=exchangename, queue='order.update_status',
+                   routing_key='order.update.*')  # Bind queue to exchange, with the routing key
 
 print(' [order_consumer] Waiting for messages.')
 
@@ -34,57 +39,68 @@ print(' [order_consumer] Waiting for messages.')
 def callback(ch, method, properties, body):
     print(" [order_consumer] Received %s" % body)
     message = json.loads(body)
-    #print(message) # has payment_intent_id
-    order = Order(id=message['order_id'], status=message['status'], user_id=None, datetime=None)
+    # print(message) # has payment_intent_id
+    order = Order(id=message['order_id'],
+                  status=message['status'], user_id=None, datetime=None)
     update_order_status(order)
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+
 def update_order_status(order):
-    try:
-        query = "UPDATE orders SET status = '{}' WHERE id = '{}'".format(order.status, order.id)
-        cursor = db.cursor()
-        cursor.execute(query)
-        db.commit()
-        cursor.close()
+    query = "UPDATE orders SET status = '{}' WHERE id = '{}'".format(
+        order.status, order.id)
+    cursor = db.cursor()
+    cursor.execute(query)
+    db.commit()
+    cursor.close()
 
-        # Send to notifications microservice
-        # default username / password to the borker are both 'guest'
-        hostname = "host.docker.internal" # default broker hostname. Web management interface default at http://localhost:15672
-        port = 5672 # default messaging port.
-        exchangename = "traval.notifications"
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
-        channel = connection.channel()
+    # Send to notifications microservice
+    # default username / password to the borker are both 'guest'
+    # default broker hostname. Web management interface default at http://localhost:15672
+    hostname = "host.docker.internal"
+    port = 5672  # default messaging port.
+    exchangename = "traval.notifications"
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=hostname, port=port))
+    channel = connection.channel()
 
-        channel.exchange_declare(exchange=exchangename, exchange_type='topic')
-        channel.queue_declare(queue='notification.notify', durable=True)
-        channel.queue_bind(exchange=exchangename, queue='notification.notify', routing_key='notify.#') # make sure the queue is bound to the exchange
-        
-        # prepares the msg to send
-        data = {
-            'order_id': order.id,
-            'status':   order.status,
-            'message_type': 'payment_success'
-        }
+    channel.exchange_declare(exchange=exchangename, exchange_type='topic')
+    channel.queue_declare(queue='notification.notify', durable=True)
+    channel.queue_bind(exchange=exchangename, queue='notification.notify',
+                       routing_key='notify.#')  # make sure the queue is bound to the exchange
 
-        message = json.dumps(data, default=str) # convert a JSON object to a string
+    # find out the deets first...
+    r = requests.get(API_URL + ":5002/orders/" + str(order.id))
+    data = json.loads(r.text)
+    r = requests.get(API_URL + ":5000/users/id/" + str(data["user_id"]))
+    data = json.loads(r.text)
 
-        # since we successfully updated the order status, send a message to notifications to send out
-        # email + sms
-        channel.basic_publish(
-            exchange=exchangename,
-            routing_key='notify.payment.success',
-            body=message,
-            properties=pika.BasicProperties(
-                delivery_mode=2,  # make message persistent
-            ))
-        connection.close()
+    # prepares the msg to send
+    data = {
+        'order_id': order.id,
+        'status':   order.status,
+        'message_type': 'payment_success',
+        'phone_number': data["phone"],
+        'email': data["email"]
+    }
 
-    except Error as error:
-        print("Failed to update record: {}".format(error))
-    # r = requests.post(API_URL + ":5002/orders/" + message['order_id'], json=message)
-    # data = json.loads(r.text)
+    # convert a JSON object to a string
+    message = json.dumps(data, default=str)
+
+    # since we successfully updated the order status, send a message to notifications to send out
+    # email + sms
+    channel.basic_publish(
+        exchange=exchangename,
+        routing_key='notify.payment.success',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    connection.close()
+
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='order.update_status', on_message_callback=callback)
+channel.basic_consume(queue='order.update_status',
+                      on_message_callback=callback)
 channel.start_consuming()
--- traval-backend/voucher/voucher_consumer.py
+++ traval-backend/voucher/voucher_consumer.py
@@ -18,17 +18,21 @@ port = 3306
 db = 'traval_vouchers'
 user = 'traval'
 password = DATABASE_PASSWORD
-db = mysql.connector.connect(host=hostname, port=port, database=db, user=user, password=password)
+db = mysql.connector.connect(
+    host=hostname, port=port, database=db, user=user, password=password)
 
 print(' [voucher_consumer] Connecting to exchange...')
 hostname = "host.docker.internal"
 port = 5672
 exchangename = "traval.payments"
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=hostname, port=port))
 channel = connection.channel()
-channel.exchange_declare(exchange=exchangename, exchange_type='topic') # Creates the exchange
+channel.exchange_declare(exchange=exchangename,
+                         exchange_type='topic')  # Creates the exchange
 channel.queue_declare(queue='voucher.create_voucher', durable=True)
-channel.queue_bind(exchange=exchangename, queue='voucher.create_voucher', routing_key='voucher.create') # make sure the queue is bound to the exchange
+channel.queue_bind(exchange=exchangename, queue='voucher.create_voucher',
+                   routing_key='voucher.create')  # make sure the queue is bound to the exchange
 
 print(' [voucher_consumer] Waiting for messages from order.update_status queue.')
 
@@ -40,56 +44,68 @@ def callback(ch, method, properties, body):
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+
 def create_voucher(message):
-    try:
-        r = requests.get(API_URL + ":5002/orders/" + message["order_id"])
-        data = json.loads(r.text)
-        time_now = datetime.datetime.now() # Get current timestamp
+    r = requests.get(API_URL + ":5002/orders/" + message["order_id"])
+    data = json.loads(r.text)
+    time_now = datetime.datetime.now()  # Get current timestamp
 
-        # get items[], loop through it, then create a voucher for each order item
-        for item in data["items"]:
-            guid = str(uuid.uuid4())
-            query = "INSERT INTO vouchers(guid, user_id, order_item_id, valid_from, status) VALUES('{}', {}, {}, '{}', '{}')".format(guid, data["user_id"], item["id"], time_now, "Unused")
-            cursor = db.cursor()
-            cursor.execute(query)
-            db.commit()
-            cursor.close()
-        
-        # Send to notifications microservice
-        # default username / password to the borker are both 'guest'
-        hostname = "host.docker.internal" # default broker hostname. Web management interface default at http://localhost:15672
-        port = 5672 # default messaging port.
-        exchangename = "traval.notifications"
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
-        channel = connection.channel()
+    # get items[], loop through it, then create a voucher for each order item
+    for item in data["items"]:
+        guid = str(uuid.uuid4())
+        query = "INSERT INTO vouchers(guid, user_id, order_item_id, valid_from, status) VALUES('{}', {}, {}, '{}', '{}')".format(
+            guid, data["user_id"], item["id"], time_now, "Unused")
+        cursor = db.cursor()
+        cursor.execute(query)
+        db.commit()
+        cursor.close()
 
-        channel.exchange_declare(exchange=exchangename, exchange_type='topic')
-        channel.queue_declare(queue='notification.notify', durable=True)
-        channel.queue_bind(exchange=exchangename, queue='notification.notify', routing_key='notify.#') # make sure the queue is bound to the exchange
-        
-        # prepares the msg to send
-        data = {
-            'order_id': message["order_id"],
-            'status':   'success',
-            'message_type': 'voucher_created'
-        }
+    # Send to notifications microservice
+    # default username / password to the borker are both 'guest'
+    # default broker hostname. Web management interface default at http://localhost:15672
+    hostname = "host.docker.internal"
+    port = 5672  # default messaging port.
+    exchangename = "traval.notifications"
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=hostname, port=port))
+    channel = connection.channel()
 
-        message = json.dumps(data, default=str) # convert a JSON object to a string
+    channel.exchange_declare(exchange=exchangename, exchange_type='topic')
+    channel.queue_declare(queue='notification.notify', durable=True)
+    channel.queue_bind(exchange=exchangename, queue='notification.notify',
+                       routing_key='notify.#')  # make sure the queue is bound to the exchange
 
-        # since we successfully created the vouchers, send a message to notifications to send out
-        # email + sms
-        channel.basic_publish(
-            exchange=exchangename,
-            routing_key='notify.voucher.created',
-            body=message,
-            properties=pika.BasicProperties(
-                delivery_mode=2,  # make message persistent
-            ))
-        connection.close()
+    # find out the deets first...
+    r = requests.get(API_URL + ":5002/orders/" + str(message["order_id"]))
+    data = json.loads(r.text)
+    r = requests.get(API_URL + ":5000/users/id/" + str(data["user_id"]))
+    data = json.loads(r.text)
 
-    except error:
-        print("Failed to update record: {}".format(error))
+    # prepares the msg to send
+    data = {
+        'order_id': message["order_id"],
+        'status':   'success',
+        'message_type': 'voucher_created',
+        'phone_number': data["phone"],
+        'email': data["email"]
+    }
+
+    # convert a JSON object to a string
+    message = json.dumps(data, default=str)
+
+    # since we successfully created the vouchers, send a message to notifications to send out
+    # email + sms
+    channel.basic_publish(
+        exchange=exchangename,
+        routing_key='notify.voucher.created',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    connection.close()
+
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='voucher.create_voucher', on_message_callback=callback)
+channel.basic_consume(queue='voucher.create_voucher',
+                      on_message_callback=callback)
 channel.start_consuming()
